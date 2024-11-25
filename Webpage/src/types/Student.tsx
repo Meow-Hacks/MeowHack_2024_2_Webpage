7a0a0314e909209da4c58dd08edc6f9e1117ff5f
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-11-26 02:22:17 +0300

    Update type student

--- Webpage/src/types/Student.tsx
+++ Webpage/src/types/Student.tsx
@@ -1,9 +1,15 @@
 export interface Student {
     isActive?: boolean; // Активность студента (пока опционально)
     id: number; // Айдишка
-    fullName: string; // ФИО
-    institute: string; // Институт
-    course: number; // Курс
-    group: string; // Группа
-    photo: string; // Фото
+    name: string;  // Имя
+    secondname: string; // Отчество
+    lastname: string; // Фамилия
+    role_id: number; // Роль
+    group_id: number; // Группа
+    institute_id: number; // Институт
+    code: string; // Код студента
+    phone: string; // Телефон
+    mail: string; // Почта
+    photo?: string; // Фото
+    course?: number;
 }
